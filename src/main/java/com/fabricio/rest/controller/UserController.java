92e88a204081117777ac03b2a35b39e3d7428311
fabricio <fabricio.sanchez.work@gmail.com>
2019-05-11 17:47:41 -0400

    Cambios para log

--- src/main/java/com/fabricio/rest/controller/UserController.java
+++ src/main/java/com/fabricio/rest/controller/UserController.java
@@ -2,6 +2,8 @@ package com.fabricio.rest.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,19 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fabricio.rest.business.UserService;
 import com.fabricio.rest.data.User;
 
+
+
 @RestController
 public class UserController {
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private UserService userService;
 	
 	@GetMapping("/users")
-	public List<User> gerUsers() {		
+	public List<User> gerUsers() {
+		logger.info("Getting users");
 		return userService.getUsers();
 	}
 	
 	@GetMapping("/user/{id}")	
-	public User gerUsers(@PathVariable Integer id) {		
+	public User gerUsers(@PathVariable Integer id) {
+		logger.info("Getting user: " + id);
 		return userService.getUser(id);
 	}
 }
